4cb82dffac6eecc32e1810e042aa3f42ce88b1a6
Unknown <drewmostoller@gmail.com>
2017-12-11 18:57:56 -0500

    woohoo

--- app/Interns.php
+++ app/Interns.php
@@ -1,10 +1,21 @@
 <?php
 
-namespace App;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Interns extends Model
-{
-    //
+$origfilename = $_FILES['app']['name'];
+$tempfilename = $_FILES['app']['tmp_name'];
+$newfiliename = "/public_html/internApps/" . $origfilename;
+$success = move_uploaded_file($tempfilename, $newfilename);
+if(!success) {
+    echo "copy failed";
 }
+else {
+    chmod($newfilename, 0644);
+    echo 'it worked!';
+}
+
+$intern = new Interns();
+$intern->filename= $newfilename;
+$intern->save();
+
+return redirect('/fundedIntern');
+
+?>
