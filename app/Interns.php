4bbd65d2d7c0369c5eaeb035a77efd925856912f
Unknown <drewmostoller@gmail.com>
2017-12-11 20:22:28 -0500

    another attempt

--- app/Interns.php
+++ app/Interns.php
@@ -2,20 +2,12 @@
 
 $origfilename = $_FILES['app']['name'];
 $tempfilename = $_FILES['app']['tmp_name'];
-$newfiliename = "/internApps/" . $origfilename;
+$newfiliename = "internApps/" . $origfilename;
 $success = move_uploaded_file($tempfilename, $newfilename);
 if(!success) {
     echo "copy failed";
 }
 else {
-    chmod($newfilename, 0644);
     echo 'it worked!';
 }
-
-$intern = new Interns();
-$intern->filename= $newfilename;
-$intern->save();
-
-return redirect('/fundedIntern');
-
 ?>
